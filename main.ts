01313b8bd79ff45e0e59d095fcb1069d98c1fb9f
ZachLian <45685256k@gmail.com>
2024-10-24 22:17:55 +0800

    basic web and plugin setting

--- main.ts
+++ main.ts
@@ -1,85 +1,29 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+// main.ts
+import { App, Plugin, PluginSettingTab, Setting, normalizePath, Notice } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface FilePathGetterPluginSettings {
+	lastUsedPath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: FilePathGetterPluginSettings = {
+	lastUsedPath: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class FilePathGetterPlugin extends Plugin {
+	settings: FilePathGetterPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		// 添加获取文件路径的命令
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+			id: 'open-file-and-get-path',
+			name: 'Open file picker and get path',
+			callback: () => this.openFilePickerAndGetPath()
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
+		// 添加设置选项卡
+		this.addSettingTab(new FilePathGetterSettingTab(this.app, this));
 	}
 
 	async loadSettings() {
@@ -89,46 +33,128 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async openFilePickerAndGetPath() {
+		// 获取 vault 中的所有文件
+		const files = this.app.vault.getFiles();
+		
+		// 创建文件选择器模态框
+		const modal = new FileSelectionModal(this.app, files, async (file: TFile) => {
+			if (file) {
+				// 获取文件的相对路径
+				const filePath = normalizePath(file.path);
+				
+				// 保存最后使用的路径
+				this.settings.lastUsedPath = filePath;
+				await this.saveSettings();
+
+				// 显示文件路径
+				new Notice(`File path: ${filePath}`);
+				
+				// 可以选择将路径复制到剪贴板
+				await navigator.clipboard.writeText(filePath);
+				new Notice('Path copied to clipboard!');
+			}
+		});
+
+		modal.open();
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+// 文件选择器模态框
+import { Modal, TFile } from 'obsidian';
+
+class FileSelectionModal extends Modal {
+	files: TFile[];
+	onChoose: (file: TFile) => void;
+	searchInput: HTMLInputElement;
+	resultContainer: HTMLDivElement;
+	filteredFiles: TFile[];
+
+	constructor(app: App, files: TFile[], onChoose: (file: TFile) => void) {
 		super(app);
+		this.files = files;
+		this.onChoose = onChoose;
+		this.filteredFiles = files;
 	}
 
 	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+		const { contentEl } = this;
+		contentEl.empty();
+
+		// 创建搜索输入框
+		this.searchInput = contentEl.createEl('input', {
+			type: 'text',
+			placeholder: 'Search for files...',
+		});
+		
+		this.searchInput.classList.add('search-input');
+
+		// 创建结果容器
+		this.resultContainer = contentEl.createEl('div');
+		this.resultContainer.classList.add('file-list-container');
+
+		// 添加搜索事件监听器
+		this.searchInput.addEventListener('input', () => {
+			this.updateFileList();
+		});
+
+		// 初始显示所有文件
+		this.updateFileList();
+
+		// 聚焦搜索框
+		this.searchInput.focus();
+	}
+
+	updateFileList() {
+		const searchTerm = this.searchInput.value.toLowerCase();
+		this.filteredFiles = this.files.filter(file => 
+			file.path.toLowerCase().includes(searchTerm)
+		);
+
+		// 清空并重新填充结果容器
+		this.resultContainer.empty();
+
+		this.filteredFiles.forEach(file => {
+			const fileItem = this.resultContainer.createEl('div', {
+				text: file.path,
+				cls: 'file-item',
+			});
+
+			fileItem.addEventListener('click', () => {
+				this.onChoose(file);
+				this.close();
+			});
+		});
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+// 设置选项卡
+class FilePathGetterSettingTab extends PluginSettingTab {
+	plugin: FilePathGetterPlugin;
 
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: FilePathGetterPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
-
+		const { containerEl } = this;
 		containerEl.empty();
 
+		containerEl.createEl('h2', { text: 'File Path Getter Settings' });
+
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Last used path')
+			.setDesc('The path of the last selected file')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+				.setPlaceholder('No file selected yet')
+				.setValue(this.plugin.settings.lastUsedPath)
+				.setDisabled(true));
 	}
 }
